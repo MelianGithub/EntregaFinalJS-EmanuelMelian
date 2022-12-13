784379bed6c2034c641bbdfebe545db8d50a4c0e
MelianGithub <e_melian04@hotmail.com>
2022-12-13 10:13:20 -0300

    Pasando todo a FETCH

--- app.js
+++ app.js
@@ -1,471 +1,219 @@
-// Array de productos
-let productos = [
-  {
-    cat: "laptop",
-    id: 1,
-    marca: "Lenovo",
-    modelo: "Legion 5 Pro",
-    cpu: "INTEL",
-    gpu: "RTX 3080",
-    RAM: 12,
-    disco: 500,
-    precio: 2000,
-    stock: 5,
-    descripcion: "Super Gaming Pc perfecta para jugar todos los juegos de pc del momento",
-    img: "../imagenes/laptoplenovo1.jpg",
-  },
-  {
-    cat: "laptop",
-    id: 2,
-    marca: "Lenovo",
-    modelo: "Legion 9",
-    cpu: "INTEL",
-    gpu: "RTX 3050",
-    RAM: 16,
-    disco: 700,
-    precio: 1200,
-    stock: 5,
-    descripcion: "Super Gaming Pc perfecta para jugar todos los juegos de pc del momento",
-    img: "../imagenes/laptoplenovo1.jpg"
+fetch('../productos.json')
+    .then(response => response.json())
+    .then((productos) => {
 
-  },
-  {
-    cat: "laptop",
-    id: 3,
-    marca: "Acer",
-    modelo: "Nitro",
-    cpu: "AMD",
-    gpu: "RX 6000",
-    RAM: 16,
-    disco: 500,
-    precio: 1050,
-    stock: 5,
-    descripcion: "Super Gaming Pc perfecta para jugar todos los juegos de pc del momento",
-    img: "../imagenes/laptoplenovo1.jpg"
+        const filtercard__container = document.querySelector("#filtercard__container");
 
-  },
-  {
-    cat: "laptop",
-    id: 4,
-    marca: "Asus",
-    modelo: "TUF",
-    cpu: "INTEL",
-    gpu: "RTX 2060",
-    RAM: 32,
-    disco: 1000,
-    precio: 3000,
-    stock: 10,
-    descripcion: "Super Gaming Pc perfecta para jugar todos los juegos de pc del momento",
-    img: "../imagenes/laptoplenovo1.jpg"
+        function cargarProductos(arraryProductos) {
+            filtercard__container.innerHTML = '';
+            for (const producto of arraryProductos) {
 
-  },
-  {
-    cat: "laptop",
-    id: 5,
-    marca: "Lenovo",
-    modelo: "Alienware",
-    cpu: "INTEL",
-    gpu: "RXT 3080",
-    RAM: 64,
-    disco: 1500,
-    precio: 1400,
-    stock: 2,
-    descripcion: "Super Gaming Pc perfecta para jugar todos los juegos de pc del momento",
-    img: "../imagenes/laptoplenovo1.jpg"
-
-  },
-  {
-    cat: "laptop",
-    id: 6,
-    marca: "MSI",
-    modelo: "GF63",
-    cpu: "INTEL",
-    gpu: "RXT 3050TI",
-    RAM: 8,
-    disco: 500,
-    precio: 7000,
-    stock: 5,
-    descripcion: "Super Gaming Pc perfecta para jugar todos los juegos de pc del momento",
-    img: "../imagenes/laptoplenovo1.jpg"
-
-  },
-  {
-    cat: "laptop",
-    id: 7,
-    marca: "MSI",
-    modelo: "GF100",
-    cpu: "INTEL",
-    gpu: "RXT 3080TI",
-    RAM: 24,
-    disco: 100,
-    precio: 800,
-    stock: 0,
-    descripcion: "Super Gaming Pc perfecta para jugar todos los juegos de pc del momento",
-    img: "../imagenes/laptoplenovo1.jpg"
-
-  },
-  {
-    cat: "laptop",
-    id: 8,
-    marca: "Hp",
-    modelo: "Victus",
-    cpu: "INTEL",
-    gpu: "RXT 2080",
-    RAM: 32,
-    disco: 100,
-    precio: 1100,
-    stock: 0,
-    descripcion: "Super Gaming Pc perfecta para jugar todos los juegos de pc del momento",
-    img: "../imagenes/laptoplenovo1.jpg"
-
-  },
-  {
-    cat: "laptop",
-    id: 9,
-    marca: "Asus",
-    modelo: "Republic of Gamers",
-    cpu: "AMD",
-    gpu: "RXT 3080",
-    RAM: 16,
-    disco: 1500,
-    precio: 500,
-    stock: 0,
-    descripcion: "Super Gaming Pc perfecta para jugar todos los juegos de pc del momento",
-    img: "../imagenes/laptoplenovo1.jpg"
-
-  },
-  {
-    cat: "laptop",
-    id: 10,
-    marca: "Asus",
-    modelo: "Moder",
-    cpu: "AMD",
-    gpu: "RXT 1660",
-    RAM: 8,
-    disco: 1500,
-    precio: 2500,
-    stock: 1,
-    descripcion: "Super Gaming Pc perfecta para jugar todos los juegos de pc del momento",
-    img: "../imagenes/laptoplenovo1.jpg"
-  },
-  {
-    cat: "laptop",
-    id: 10,
-    marca: "Dell",
-    modelo: "Nose",
-    cpu: "AMD",
-    gpu: "RXT 1660",
-    RAM: 8,
-    disco: 1500,
-    precio: 2600,
-    stock: 4,
-    descripcion: "Super Gaming Pc perfecta para jugar todos los juegos de pc del momento",
-    img: "../imagenes/laptoplenovo1.jpg"
-  },
-  {
-    cat: "laptop",
-    id: 10,
-    marca: "Dell",
-    modelo: "Precision",
-    cpu: "INTEL",
-    gpu: "RXT 1660",
-    RAM: 8,
-    disco: 1500,
-    precio: 900,
-    stock: 1,
-    descripcion: "Super Gaming Pc perfecta para jugar todos los juegos de pc del momento",
-    img: "../imagenes/laptoplenovo1.jpg"
-  },
-  {
-    cat: "monitor",
-    id: 10,
-    marca: "MSI",
-    modelo: "Optix",
-    precio: 269,
-    stock: 4,
-    descripcion: "Con su diseño sin bordes, este monitor te brinda la mejor experiencia de juego.",
-    img: ".jpg"
-  },
-  {
-    cat: "monitor",
-    id: 10,
-    marca: "MSI",
-    modelo: "Optix",
-    precio: 119,
-    stock: 4,
-    descripcion: "Con su diseño sin bordes, este monitor te brinda la mejor experiencia de juego.",
-    img: ".jpg"
-  },
-  {
-    cat: "monitor",
-    id: 10,
-    marca: "MSI",
-    modelo: "Optix",
-    precio: 300,
-    stock: 4,
-    descripcion: "Con su diseño sin bordes, este monitor te brinda la mejor experiencia de juego.",
-    img: ".jpg"
-  },
-  {
-    cat: "monitor",
-    id: 10,
-    marca: "MSI",
-    modelo: "Optix",
-    precio: 269,
-    stock: 4,
-    descripcion: "Con su diseño sin bordes, este monitor te brinda la mejor experiencia de juego.",
-    img: ".jpg"
-  },
-  {
-    cat: "monitor",
-    id: 10,
-    marca: "MSI",
-    modelo: "Optix",
-    precio: 249,
-    stock: 4,
-    descripcion: "Con su diseño sin bordes, este monitor te brinda la mejor experiencia de juego.",
-    img: ".jpg"
-  },
-  {
-    cat: "monitor",
-    id: 10,
-    marca: "MSI",
-    modelo: "Optix",
-    precio: 300,
-    stock: 4,
-    descripcion: "Con su diseño sin bordes, este monitor te brinda la mejor experiencia de juego.",
-    img: ".jpg"
-  },
-  {
-    cat: "monitor",
-    id: 10,
-    marca: "MSI",
-    modelo: "Optix",
-    precio: 500,
-    stock: 4,
-    descripcion: "Con su diseño sin bordes, este monitor te brinda la mejor experiencia de juego.",
-    img: ".jpg"
-  },
-  // GRAFICAS
-  {
-    cat: "grafica",
-    id: 10,
-    marca: "NVIDIA",
-    modelo: "RTX 2060",
-    precio: 700,
-    stock: 3,
-    descripcion: "Ideal para gaming",
-    img: ".jpg"
-  },
-  {
-    cat: "grafica",
-    id: 10,
-    marca: "NVIDIA",
-    modelo: "RTX 2070",
-    precio: 800,
-    stock: 4,
-    descripcion: "Ideal para gaming",
-    img: ".jpg"
-  },
-  {
-    cat: "grafica",
-    id: 10,
-    marca: "NVIDIA",
-    modelo: "RTX 2080",
-    precio: 900,
-    stock: 1,
-    descripcion: "Ideal para gaming",
-    img: ".jpg"
-  },
-  {
-    cat: "grafica",
-    id: 10,
-    marca: "RADEON",
-    modelo: "RX 6700 XT",
-    precio: 1050,
-    stock: 1,
-    descripcion: "Ideal para gaming",
-    img: ".jpg"
-  },
-  {
-    cat: "grafica",
-    id: 10,
-    marca: "RADEON",
-    modelo: "RX 6400",
-    precio: 900,
-    stock: 6,
-    descripcion: "Ideal para gaming",
-    img: ".jpg"
-  },
-  {
-    cat: "grafica",
-    id: 10,
-    marca: "RADEON",
-    modelo: "RX 6500",
-    precio: 750,
-    stock: 12,
-    descripcion: "Ideal para gaming",
-    img: ".jpg"
-  },
-
-];
-
-// const buscarItemIndex = document.querySelector("#buscarIndex");
-// buscarItemIndex.addEventListener("change", buscadorItemIndex);
-
-// function buscadorItemIndex() {
-//   const buscar = productos.filter(producto => {
-//     return producto.stock > 0 && producto.marca.toLocaleLowerCase() === buscarItemIndex.value
-//       || producto.modelo.toLocaleLowerCase() === buscarItemIndex.value;
-//   });
-
-
-//   console.log(buscar)
-
-//   localStorage.setItem("productos-index", JSON.stringify(buscar))
-// }
-
-
-// renderizar();
-const contenedorProductos = document.querySelector("#contenedorProductos");
-
-function renderizar() {
-  for (const producto of productos) {
-
-    let unidades = "Agregar"
-    if (producto.stock <= 0) {
-      unidades = "Sin Stock"
-    }
-
-    let tarjetaProducto = document.createElement("div");
-    tarjetaProducto.className = "card";
-
-    tarjetaProducto.innerHTML = `
-  <img src=${producto.img} class="card__img">
-  <h3>${producto.marca}</h3>
-  <p class = "text__card">${producto.descripcion} </p>
-  <h4>$${producto.precio}</h4>
-  <button class= "bt__comprar">${unidades}</button>
-  `;
-    contenedorProductos.append(tarjetaProducto);
-  }
-}
-
-// pagina productos
-
-const filtercard__container = document.querySelector("#filtercard__container");
-
-function cargarProductos(arraryProductos) {
-  filtercard__container.innerHTML = '';
-  for (const producto of arraryProductos) {
-
-    let unidades = "Agregar"
-    if (producto.stock <= 0) {
-      unidades = "Sin Stock"
-    }
-    const mostrarProductos = document.createElement("div");
-    mostrarProductos.className = "card";
-    mostrarProductos.innerHTML = `
+                let unidades = "Agregar"
+                if (producto.stock <= 0) {
+                    unidades = "Sin Stock"
+                }
+                const mostrarProductos = document.createElement("div");
+                mostrarProductos.className = "card";
+                mostrarProductos.innerHTML = `
     <img src=${producto.img} class="card__img">
     <h3>${producto.marca}</h3>
     <p class = "text__card">${producto.descripcion} </p>
     <h4>$${producto.precio}</h4>
    <button class= "producto-agregar" id="${producto.id}">${unidades}</button>
   `;
-    filtercard__container.append(mostrarProductos);
-  }
-  actualizarBotonesAgregar();
-};
+                filtercard__container.append(mostrarProductos);
+            }
+            actualizarBotonesAgregar();
+        };
 
-// funcion error
-function error() {
-  const mensajeError = document.createElement("div");
-  filtercard__container.innerHTML = " ";
-  mensajeError.innerHTML = `<h3>No se encontraron resultados</h3>`;
+        // funcion error
+        function error() {
+            const mensajeError = document.createElement("div");
+            filtercard__container.innerHTML = " ";
+            mensajeError.innerHTML = `<h3>No se encontraron resultados</h3>`;
 
-  filtercard__container.append(mensajeError);
-}
+            filtercard__container.append(mensajeError);
+        }
 
-// funcion buscar
 
-const buscarItem = document.querySelector("#buscar");
-buscarItem.addEventListener("change", buscadorItem);
+        // funcion buscar
 
-function buscadorItem() {
-  const buscar = productos.filter(producto => {
-    return producto.stock > 0
-      && producto.marca.toLocaleLowerCase() === buscarItem.value
-      || producto.modelo.toLocaleLowerCase() === buscarItem.value
-      || producto.cat.toLocaleLowerCase() === buscarItem.value;
-  });
+        const buscarItem = document.querySelector("#buscar");
+        buscarItem.addEventListener("change", buscadorItem);
 
-  buscar.length > 0 ? cargarProductos(buscar) : error(); // ESTO ES LO MISMO QUE UN IF ELSE
+        function buscadorItem() {
+            let buscar = productos.filter((producto) => {
+                return producto.marca.toLowerCase() == buscarItem.value.toLowerCase()||
+                producto.cat.toLowerCase() == buscarItem.value.toLowerCase()|| 
+                producto.cpu.toLowerCase() == buscarItem.value.toLowerCase()
 
-}
+            })
 
-// AGREGAR AL CARRITO
+            buscar.length > 0 ? cargarProductos(buscar) : error(); // ESTO ES LO MISMO QUE UN IF ELSE
 
-let botonesAgregar = document.querySelectorAll(".producto-agregar");
+        }
 
-function actualizarBotonesAgregar() {
-  botonesAgregar = document.querySelectorAll(".producto-agregar");
 
-  botonesAgregar.forEach(boton => {
-    boton.addEventListener("click", agregarAlCarrito);
-  });
-}
+        // AGREGAR AL CARRITO
 
-function agregarAlCarrito(e) {
-  const idBoton = e.currentTarget.id;
-  const productoAgregado = productos.find(producto => producto.id == idBoton);
+        let botonesAgregar = document.querySelectorAll(".producto-agregar");
 
-  if (productosEnCarrito.some(producto => producto.id == idBoton)) {
-    const index = productosEnCarrito.findIndex(producto => producto.id == idBoton)
-    productosEnCarrito[index].cantidad++;
-  } else {
-    productoAgregado.cantidad = 1;
-    productosEnCarrito.push(productoAgregado);
-  }
-  actualizarNumerito();
-  toa();
+        function actualizarBotonesAgregar() {
+            botonesAgregar = document.querySelectorAll(".producto-agregar");
 
-  // LOCAL STORAGE
-  localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
-}
+            botonesAgregar.forEach(boton => {
+                boton.addEventListener("click", agregarAlCarrito);
+            });
+        }
 
-// Numero de Carrito
-const numerito = document.querySelector("#numerito")
+        function agregarAlCarrito(e) {
+            const idBoton = e.currentTarget.id;
+            const productoAgregado = productos.find(producto => producto.id == idBoton);
 
-function actualizarNumerito() {
-  let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
-  numerito.innerText = nuevoNumerito;
-  
-}
+            if (productosEnCarrito.some(producto => producto.id == idBoton)) {
+                const index = productosEnCarrito.findIndex(producto => producto.id == idBoton)
+                productosEnCarrito[index].cantidad++;
+            } else {
+                productoAgregado.cantidad = 1;
+                productosEnCarrito.push(productoAgregado);
+            }
+            actualizarNumerito();
+            toa();
 
-// ------------------
-let productosEnCarrito;
-let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+            // LOCAL STORAGE
+            localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+        }
 
-if (productosEnCarritoLS) {
-  productosEnCarrito = JSON.parse(productosEnCarritoLS);
-  actualizarNumerito();
-} else {
-  productosEnCarrito = [];
-}
-// Toastify Notifications 
+        // Numero de Carrito
+        const numerito = document.querySelector("#numerito")
 
-function toa() {
-  Toastify({
-      text: "Agregaste un producto al carrito",
-      duration: 1000,
-      newWindow: true,
-      close: true,
-      gravity: "top", // `top` or `bottom`
-      position: "right", // `left`, `center` or `right`
-      stopOnFocus: true, // Prevents dismissing of toast on hover
-      style: {
-        background: "linear-gradient(to right, #00b09b, #96c93d)",
-      },
-      onClick: function(){} // Callback after click
-    }).showToast();
-}
+        function actualizarNumerito() {
+            let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+            numerito.innerText = nuevoNumerito;
+
+        }
+
+        // ------------------
+        let productosEnCarrito;
+        let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+        if (productosEnCarritoLS) {
+            productosEnCarrito = JSON.parse(productosEnCarritoLS);
+            actualizarNumerito();
+        } else {
+            productosEnCarrito = [];
+        }
+        // Toastify Notifications 
+
+        function toa() {
+            Toastify({
+                text: "Agregaste un producto al carrito",
+                duration: 1000,
+                newWindow: true,
+                close: true,
+                gravity: "top", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                    background: "linear-gradient(to right, #00b09b, #96c93d)",
+                },
+                onClick: function () { } // Callback after click
+            }).showToast();
+        }
+
+
+
+        // -----------------------------adadad--------------------
+
+
+        const filter = document.querySelector("#filter");
+        const botonesCategorias = document.querySelectorAll(".botones__categorias");
+        const selectOrder = document.querySelector("#selectOrder");
+        const priceMax = document.querySelector("#my-range");
+        const linkLaptop = document.querySelector("#linkLaptop")
+
+
+        function cargarProductosFilter(arraryProductos) {
+            filter.innerHTML = '';
+
+            for (const producto of arraryProductos) {
+
+                let unidades = "Agregar"
+                if (producto.stock <= 0) {
+                    unidades = "Sin Stock"
+                }
+
+                const mostrarProductos = document.createElement("div");
+                mostrarProductos.className = "card";
+                mostrarProductos.innerHTML = `
+      <img src=${producto.img} class="card__img">
+      <h3>${producto.marca}</h3>
+      <p class = "text__card">${producto.descripcion} </p>
+      <h4>$${producto.precio}</h4>
+     <button class= "producto-agregar" id="${producto.id}">${unidades}</button>
+    `;
+                filter.append(mostrarProductos);
+            }
+            actualizarBotonesAgregar();
+        };
+
+        cargarProductosFilter(productos);
+        filtrarPrecio(productos);
+        ordenadoPor(productos);
+
+
+        botonesCategorias.forEach((boton) => {
+            boton.addEventListener("click", (e) => {
+                filter.innerHTML = '';
+                const buscados = productos.filter(
+                    (producto) =>
+                        producto.cpu === e.currentTarget.id ||
+                        producto.cat === e.currentTarget.id ||
+                        producto.marca === e.currentTarget.id
+                );
+
+                cargarProductosFilter(buscados);
+                ordenadoPor(buscados);
+                filtrarPrecio(buscados);
+
+            });
+
+        });
+
+
+        function ordenadoPor(array) {
+            selectOrder.addEventListener("change", () => {
+                const ordenadoPorPrecio = array.sort((a, b) => {
+                    if (a.precio < b.precio) {
+                        return 1
+                    } else if (a.precio > b.precio) {
+                        return -1
+                    } else {
+                        return 0
+                    }
+                });
+
+                let selectedOption = selectOrder.options[selectOrder.selectedIndex];
+                if (selectedOption.value == 1) {
+                    console.log('Selected option: ' + selectedOption.value);
+                    cargarProductosFilter(ordenadoPorPrecio);
+                } else if (selectedOption.value == 2) {
+                    cargarProductosFilter(ordenadoPorPrecio.reverse());
+                }
+            });
+        };
+
+        // FILTRAR RANGO PRECIOS
+
+        function filtrarPrecio(arrayPrecios) {
+            priceMax.max = "9000";
+            priceMax.addEventListener("click", (e) => {
+                const precioMaximo = arrayPrecios.filter(
+                    (producto) =>
+                        producto.precio <= e.currentTarget.value
+                );
+                console.log(e.currentTarget.value)
+                cargarProductosFilter(precioMaximo);
+            })
+        }
+    })
